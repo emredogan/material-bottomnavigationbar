d46aed7d6d86f6d43ce2e716437f886e2304b4db
Emre <emredogan.boun@gmail.com>
2018-03-10 19:20:59 +0100

    bottom nav bar

---
+++ app/src/main/java/com/emredogan/bottomnavigation/MainActivity.java
@@ -0,0 +1,61 @@
+package com.emredogan.bottomnavigation;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigation_id);
+        txt = (TextView) findViewById(R.id.text_id);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+                int id = item.getItemId();
+
+                switch (id)  {
+                    case R.id.recents_id:
+
+                        txt.setText(getResources().getText(R.string.recents));
+
+                        return true;
+
+
+                    case R.id.favorites_id:
+
+                        txt.setText(getResources().getText(R.string.favorites));
+
+                        return true;
+
+                    case R.id.nearby_id:
+
+                        txt.setText(getResources().getText(R.string.near_by));
+
+                        return true;
+
+
+                }
+
+                return false;
+            }
+        });
+
+
+    }
+
+
+}
